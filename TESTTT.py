a8b6845dbe00c2c5f32296bfc65970032780193e
MaiKau <56115762+MaiKau@users.noreply.github.com>
2021-12-02 15:14:15 +0800

    Add files via upload

---
+++ TESTTT.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for, request, render_template, session, jsonify
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False #json格式亂碼，改成ascii碼
+
+@app.route("/", methods=['POST'])
+def test(A):
+	get_JsonInput = request.data.decode()
+	b=get_JsonInput + "1111" +A
+	return b
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug = False, port=5000, threaded=True)
